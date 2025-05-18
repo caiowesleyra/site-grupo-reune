ec4a0f5bc4f07ea55aca8666c76111a8c3c673be
Caio Wesley <g8pro.rede@gmail.com>
2025-05-18 02:28:24 -0300

    Conectando ao banco da Hostinger

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,17 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'srv1896.hstgr.io',       // <-- substitua pelo host exato do banco (provavelmente algo assim)
+  user: 'u167429742_adminreune', // <-- seu usuário
+  password: '270389Caw@',         // <-- sua senha do MySQL
+  database: 'u167429742_gruporeune_db' // <-- nome do banco
 });
 
 db.connect((err) => {
   if (err) {
     console.error('Erro ao conectar no banco de dados:', err);
   } else {
-    console.log('Conectado ao banco de dados com sucesso!');
+    console.log('🟢 Banco de dados conectado com sucesso!');
   }
 });
 
